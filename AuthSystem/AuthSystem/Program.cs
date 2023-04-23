e7a4f7b4df8584c8e3862b3011f9ff3654384829
Edini Amare Getachew <122861398+e-d-i-n-i@users.noreply.github.com>
2023-04-23 19:19:42 +0300

    Register UI and Functionality Completed

--- AuthSystem/AuthSystem/Program.cs
+++ AuthSystem/AuthSystem/Program.cs
@@ -8,7 +8,7 @@ var connectionString = builder.Configuration.GetConnectionString("AuthDbContextC
 
 builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AuthDbContext>();
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<AuthDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
